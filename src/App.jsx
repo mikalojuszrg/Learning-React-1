b3207015744627e5306be17f0a848840ac4f1472
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-01-10 18:00:06 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import Hero from "./components/Hero/Hero";
-import MainSection from "./components/MainSection/MainSection";
 
 function App() {
-  return (
-    <div className="App">
-      <Hero title="Title" description="Some description some descript" />
-      <MainSection
-        title="Portfolio"
-        description="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
